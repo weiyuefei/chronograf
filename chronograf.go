9bdbf2f1e9b186bb4b5b8cc1d6b63e7f1846f92e
Chris Goller <goller@gmail.com>
2018-02-20 20:32:46 -0600

    Add measurements with pagination

--- chronograf.go
+++ chronograf.go
@@ -458,6 +458,11 @@ type RetentionPolicy struct {
 	Default       bool   `json:"isDefault,omitempty"`     // whether the RP should be the default
 }
 
+// Measurement represents a measurement in a time series source
+type Measurement struct {
+	Name string `json:"name"` // a unique string identifier for the measurement
+}
+
 // Databases represents a databases in a time series source
 type Databases interface {
 	// All lists all databases
@@ -465,10 +470,13 @@ type Databases interface {
 	Connect(context.Context, *Source) error
 	CreateDB(context.Context, *Database) (*Database, error)
 	DropDB(context.Context, string) error
+
 	AllRP(context.Context, string) ([]RetentionPolicy, error)
 	CreateRP(context.Context, string, *RetentionPolicy) (*RetentionPolicy, error)
 	UpdateRP(context.Context, string, string, *RetentionPolicy) (*RetentionPolicy, error)
 	DropRP(context.Context, string, string) error
+
+	AllMeasurements(ctx context.Context, db string, limit, offset int) ([]Measurement, error)
 }
 
 // DashboardID is the dashboard ID
